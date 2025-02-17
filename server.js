f414f2c0159883c7bbfa785fa9dd3f4d914be2bd
singhsamiksha <singhsamiksha877@gmail.com>
2025-02-17 16:59:40 +0530

    Added ui for admin and index

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const router = require("./routes/article.route.js");
 const mongoose = require("mongoose");
+const cors = require("cors");
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 const port = 5000;
 
 mongoose.connect("mongodb://localhost:27017/personalblog");
